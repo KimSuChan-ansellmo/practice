d70137109bc55791446ed93444ca92c61e5792e2
DESKTOP-9HFBP0P\ksc <ksc@DESKTOP-9HFBP0P>
2022-11-16 21:25:15 +0900

    1st

---
+++ ksc/src/test/java/com/ksc/KscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
